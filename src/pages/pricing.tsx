3b1114426c6b6b73b965a430d3991fdd3d3761ee
Simon VDB <simonvdbroeck@gmail.com>
2021-01-16 13:28:08 +0100

    Update pricing.tsx

--- src/pages/pricing.tsx
+++ src/pages/pricing.tsx
@@ -40,7 +40,7 @@ function PricingDetails() {
                         <div className="mt-4 flex items-center justify-center">
                           <span className="px-3 flex items-start text-6xl tracking-tight text-gray-900">
                             <span className="mt-2 mr-2 text-4xl font-medium">€</span>
-                            <span className="font-extrabold">79</span>
+                            <span className="font-extrabold">69</span>
                           </span>
                           <span className="text-xl font-medium text-gray-500">/maand</span>
                         </div>
