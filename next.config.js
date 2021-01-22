624c0ee765ecd8aa06a31056dcad33a453ee5c05
Simon VDB <simonvdbroeck@gmail.com>
2021-01-22 01:14:03 +0100

    optimize font

--- next.config.js
+++ next.config.js
@@ -6,6 +6,19 @@ const nextConfig = {
   images: {
     domains: ['tailwindui.com', 'images.unsplash.com'],
   },
+  async headers() {
+    return [
+      {
+        source: '/fonts/inter-var-latin.woff2',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'public, max-age=31536000, immutable',
+          },
+        ],
+      },
+    ];
+  },
   async redirects() {
     return [];
   },
