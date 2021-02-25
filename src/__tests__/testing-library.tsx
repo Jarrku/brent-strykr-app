64bd5b0d0eca746085996376faa57301d049af3d
Simon VDB <simonvdbroeck@gmail.com>
2021-02-25 23:14:07 +0100

    fix testing

--- src/__tests__/testing-library.tsx
+++ src/__tests__/testing-library.tsx
@@ -1,10 +1,8 @@
-import React from 'react'
-import {render} from '@testing-library/react'
+import React from 'react';
+import { render } from '@testing-library/preact';
 
 test('renders deploy link', () => {
-  const {getByText} = render(
-    <h1 id="welcome-to-the-site">Welcome to the Site</h1>,
-  )
-  const linkElement = getByText(/Welcome to the Site/)
-  expect(linkElement).toBeInTheDocument()
-})
+  const { getByText } = render(<h1 id="welcome-to-the-site">Welcome to the Site</h1>);
+  const linkElement = getByText(/Welcome to the Site/);
+  expect(linkElement).toBeInTheDocument();
+});
