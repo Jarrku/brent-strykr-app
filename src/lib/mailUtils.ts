e526698e43fb4edea5091ca36d2ae835bbc0237c
Simon Van den Broeck <simonvdbroeck@gmail.com>
2021-03-12 12:46:20 +0100

    add mail utils

---
+++ src/lib/mailUtils.ts
@@ -0,0 +1,74 @@
+import {
+  TransactionalEmailsApi,
+  TransactionalEmailsApiApiKeys,
+  SendSmtpEmail,
+  SendSmtpEmailTo,
+  SendSmtpEmailSender,
+  SendSmtpEmailReplyTo,
+  SendSmtpEmailAttachment,
+} from 'sib-api-v3-typescript';
+
+const smtpApi = new TransactionalEmailsApi();
+smtpApi.setApiKey(TransactionalEmailsApiApiKeys.apiKey, process.env.SENDINBLUE_API_KEY);
+
+interface Props {
+  body: string;
+  replyTo: string;
+  replyToName?: string;
+  to: string;
+  toName?: string;
+  sender: string;
+  senderName?: string;
+  subject: string;
+  attachmentUrl?: string;
+}
+
+export function sendEmail({
+  body,
+  replyTo,
+  replyToName,
+  to,
+  toName,
+  sender,
+  senderName,
+  subject,
+  attachmentUrl,
+}: Props) {
+  const smtpEmail = new SendSmtpEmail();
+
+  smtpEmail.to = [createReceiver(to, toName)];
+  smtpEmail.sender = createSender(sender, senderName);
+  smtpEmail.subject = subject;
+  smtpEmail.replyTo = createReplyTo(replyTo, replyToName);
+  smtpEmail.textContent = body;
+  smtpEmail.attachment = attachmentUrl ? [createAttachment(attachmentUrl)] : undefined;
+
+  return smtpApi.sendTransacEmail(smtpEmail);
+}
+
+function createSender(email: string, name?: string) {
+  const sender = new SendSmtpEmailSender();
+  sender.email = email;
+  sender.name = name;
+  return sender;
+}
+
+function createReceiver(email: string, name?: string) {
+  const receiver = new SendSmtpEmailTo();
+  receiver.email = email;
+  receiver.name = name;
+  return receiver;
+}
+
+function createReplyTo(email: string, name?: string) {
+  const receiver = new SendSmtpEmailReplyTo();
+  receiver.email = email;
+  receiver.name = name;
+  return receiver;
+}
+
+function createAttachment(url: string) {
+  const attachment = new SendSmtpEmailAttachment();
+  attachment.url = url;
+  return attachment;
+}
