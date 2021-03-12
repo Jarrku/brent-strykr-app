e6d166a821beb1fe278fe12c845cf93dbd6f812e
Simon Van den Broeck <simonvdbroeck@gmail.com>
2021-03-12 20:20:54 +0100

    fix sib wacky build

--- src/lib/mailUtils.ts
+++ src/lib/mailUtils.ts
@@ -1,16 +1,18 @@
-import {
-  TransactionalEmailsApi,
-  TransactionalEmailsApiApiKeys,
-  SendSmtpEmail,
-  SendSmtpEmailTo,
-  SendSmtpEmailSender,
-  SendSmtpEmailReplyTo,
-  SendSmtpEmailAttachment,
-} from 'sib-api-v3-typescript';
+// import {
+//   TransactionalEmailsApi,
+//   TransactionalEmailsApiApiKeys,
+//   SendSmtpEmail,
+//   SendSmtpEmailTo,
+//   SendSmtpEmailSender,
+//   SendSmtpEmailReplyTo,
+//   SendSmtpEmailAttachment,
+// } from 'sib-api-v3-typescript';
 import { EmailProps } from './types';
+//TODO: Fix when https://github.com/sendinblue/APIv3-typescript-library/issues/16 is merged
+const sib = require('sib-api-v3-typescript');
 
-const smtpApi = new TransactionalEmailsApi();
-smtpApi.setApiKey(TransactionalEmailsApiApiKeys.apiKey, process.env.SENDINBLUE_API_KEY);
+const smtpApi = new sib.TransactionalEmailsApi();
+smtpApi.setApiKey(sib.TransactionalEmailsApiApiKeys.apiKey, process.env.SENDINBLUE_API_KEY);
 
 export function sendEmail({
   body,
@@ -23,7 +25,7 @@ export function sendEmail({
   subject,
   attachmentUrl,
 }: EmailProps) {
-  const smtpEmail = new SendSmtpEmail();
+  const smtpEmail = new sib.SendSmtpEmail();
 
   smtpEmail.to = [createReceiver(to, toName)];
   smtpEmail.sender = createSender(sender, senderName);
@@ -36,28 +38,28 @@ export function sendEmail({
 }
 
 function createSender(email: string, name?: string) {
-  const sender = new SendSmtpEmailSender();
+  const sender = new sib.SendSmtpEmailSender();
   sender.email = email;
   sender.name = name;
   return sender;
 }
 
 function createReceiver(email: string, name?: string) {
-  const receiver = new SendSmtpEmailTo();
+  const receiver = new sib.SendSmtpEmailTo();
   receiver.email = email;
   receiver.name = name;
   return receiver;
 }
 
 function createReplyTo(email: string, name?: string) {
-  const receiver = new SendSmtpEmailReplyTo();
+  const receiver = new sib.SendSmtpEmailReplyTo();
   receiver.email = email;
   receiver.name = name;
   return receiver;
 }
 
 function createAttachment(url: string) {
-  const attachment = new SendSmtpEmailAttachment();
+  const attachment = new sib.SendSmtpEmailAttachment();
   attachment.url = url;
   return attachment;
 }
