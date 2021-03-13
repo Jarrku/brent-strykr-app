98aadc7d31ed58f43bf2c6b70c63e0bb7203973e
Simon Van den Broeck <simonvdbroeck@gmail.com>
2021-03-13 16:03:59 +0100

    add studio files

---
+++ studio/src/deskStructure/siteSettings.js
@@ -0,0 +1,7 @@
+import S from '@sanity/desk-tool/structure-builder';
+import { MdSettings } from 'react-icons/md';
+
+export default S.listItem()
+  .title('Settings')
+  .child(S.document().id('siteSettings').schemaType('siteSettings').documentId('siteSettings'))
+  .icon(MdSettings);
